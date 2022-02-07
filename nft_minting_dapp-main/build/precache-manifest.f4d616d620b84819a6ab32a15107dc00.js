7b6be9e6fe3bd4480ec1ac218337ed5d867b2a6f
ipoulter <44752303+ipoulter@users.noreply.github.com>
2022-02-06 20:34:49 -0600

    Add files via upload

---
+++ nft_minting_dapp-main/build/precache-manifest.f4d616d620b84819a6ab32a15107dc00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e215512802dd41ff0b6169276e253f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4da46c6c2dffbe529c6",
+    "url": "/static/css/main.9136e423.chunk.css"
+  },
+  {
+    "revision": "13a76ab3d8c850d48a93",
+    "url": "/static/js/2.2fdcd4ad.chunk.js"
+  },
+  {
+    "revision": "9141e6603b2ba65e9f944cb50ce12050",
+    "url": "/static/js/2.2fdcd4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af658ec7d3c94ccd0e8",
+    "url": "/static/js/3.e8ff46fa.chunk.js"
+  },
+  {
+    "revision": "f4da46c6c2dffbe529c6",
+    "url": "/static/js/main.73a35056.chunk.js"
+  },
+  {
+    "revision": "513cbd3bb045ee6290d5",
+    "url": "/static/js/runtime-main.5ee2c4fe.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  }
+]);
